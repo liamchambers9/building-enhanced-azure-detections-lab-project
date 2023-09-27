cee8e101d88e3b0c8a06369f15be7a292c3d8d0b
Liam Chambers <101218893+liamchambers9@users.noreply.github.com>
2023-09-27 14:27:38 -0400

    Create users.tf

---
+++ terraform/users.tf
@@ -0,0 +1,32 @@
+resource "azuread_application" "storage_manager" {
+  display_name = "Storage Manager"
+  owners       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal" "storage_manager_sp" {
+  application_id               = azuread_application.storage_manager.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal_password" "storage_manager_password" {
+  service_principal_id = azuread_service_principal.storage_manager_sp.object_id
+}
+
+resource "azurerm_role_assignment" "store_manager_storage_blob_data_reader" {
+  scope                = azurerm_storage_account.prod_data.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = azuread_service_principal.storage_manager_sp.id
+}
+
+resource "azurerm_role_assignment" "store_manager_storage_account_reader" {
+  scope                = azurerm_storage_account.prod_data.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_service_principal.storage_manager_sp.id
+}
+
+resource "azurerm_role_assignment" "current_user_storage_blob_data_reader" {
+  scope                = azurerm_storage_account.prod_data.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = data.azuread_client_config.current.object_id
+}
